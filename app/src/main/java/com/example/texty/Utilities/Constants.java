2f8b2403e17f5afffcfe6439600d0826e8ba3b22
Ahmed Ibrahim <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2019-04-29 01:16:09 +0200

    Add log out functionality

--- app/src/main/java/com/example/texty/Utilities/Constants.java
+++ app/src/main/java/com/example/texty/Utilities/Constants.java
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 package com.example.texty.Utilities;
 
 public class Constants {
 
     public final static String SIGN_IN_API = "";
     public final static String SIGN_UP_API = "";
-    public final static String CHAT_ROOM_API = "http://";
+    public final static String CHAT_ROOM_API = "http://4a755446.ngrok.io";
 
 }
-=======
-package com.example.texty.Utilities;
-
-public class Constants {
-
-    public final static String SIGN_IN_API = "";
-    public final static String SIGN_UP_API = "";
-    public final static String CHAT_ROOM_API = "http://192.168.1.21:3000";
-
-}
->>>>>>> d461f352a595a5bb8def4fc5780408cbf39871ca
