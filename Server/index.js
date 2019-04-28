e22b94eecf30f4ace6564d7375b9a4633389fcb6
Ahmed Ibrahim <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2019-04-29 01:40:38 +0200

    Add server checkers

--- Server/index.js
+++ Server/index.js
@@ -40,40 +40,48 @@ io.on('connection', function(socket){
     console.log(username + ' set user name');
     userIDs.set(username,socket.id);
     usernames.set(socket.id,username);
-    socket.broadcast.emit("user join",username);
-    usersList.push(username);
+    if(!usersList.includes(username)){
+      usersList.push(username);
+      socket.broadcast.emit("user join",username);
+    }
     io.to(socket.id).emit("retrieve list",usersList);
   });
 
   socket.on('disconnect', function(){
-    var username = usernames.get(socket.id);
-    console.log(username + ' disconnected');
-    usernames.delete(socket.id);
-    userIDs.delete(username);
-    usersList = usersList.filter(function(value, index, arr){ return value !== username;});
-    socket.broadcast.emit("user leave",username);
+    if(usernames.has(socket.id)){
+      var username = usernames.get(socket.id);
+      console.log(username + ' disconnected');
+      usernames.delete(socket.id);
+      userIDs.delete(username);
+      usersList = usersList.filter(function(value, index, arr){ return value !== username;});
+      socket.broadcast.emit("user leave",username);
+    }
   });
 
   socket.on('group message', function(message){
-    var username = usernames.get(socket.id);
-    var dataSent = {"username":username,"message":message};
-    socket.broadcast.emit('group message', dataSent);
-    console.log('message: ' + message);
+    if(usernames.has(socket.id)){
+      var username = usernames.get(socket.id);
+      var dataSent = {"username":username,"message":message};
+      socket.broadcast.emit('group message', dataSent);
+      console.log('message: ' + message);
+    }
   });
 
   socket.on('private message', function(data){
     var to = data["to"];
     var message = data["message"];
-    var username = usernames.get(socket.id);
 
-    //if user is online
-    if(userIDs.has(to)){
-      var dataSent = {"username":username,"message":message};
-      console.log(dataSent);
-      console.log(to);
-      io.to(userIDs.get(to)).emit('private message', dataSent);
+    if(usernames.has(socket.id)){
+      var username = usernames.get(socket.id);
+      //if user is online
+      if(userIDs.has(to)){
+        var dataSent = {"username":username,"message":message};
+        console.log(dataSent);
+        console.log(to);
+        io.to(userIDs.get(to)).emit('private message', dataSent);
+      }
+      console.log('message: ' + message);
     }
-    console.log('message: ' + message);
   });
   
 });
